348f3831b4f4b270c454f745d98e6fa48e016335
ignvcio42 <ignacio.guajardo_m@mail.udp.cl>
2023-09-01 23:39:50 -0400

    actualizacion

--- Lab_1/cesar.py
+++ Lab_1/cesar.py
@@ -1,21 +1,28 @@
-def cifrar_cesar(texto, correccion):
-    resultado = ""
-    for letra in texto:
-        if letra.isalpha():  # Solo cifrar letras del alfabeto
-            ajuste = 65 if letra.isupper() else 97  # 65 es el valor ASCII de 'A', 97 de 'a'
-            cifrado = (ord(letra) - ajuste + correccion) % 26
-            resultado += chr(cifrado + ajuste)
+import sys
+
+def cifrar_cesar(mensaje, corrimiento):
+    mensaje_cifrado = ""
+    for caracter in mensaje:
+        if caracter.isalpha():
+            mayuscula = caracter.isupper()
+            caracter = caracter.upper()
+            codigo = ord(caracter)
+            codigo_cifrado = ((codigo - 65 + corrimiento) % 26) + 65
+            if not mayuscula:
+                mensaje_cifrado += chr(codigo_cifrado).lower()
+            else:
+                mensaje_cifrado += chr(codigo_cifrado)
         else:
-            resultado += letra  # Conserva los caracteres que no son letras
-    return resultado
-
-def main():
-    texto_original = input("Ingrese el texto a cifrar: ")
-    correccion = int(input("Ingrese la corrección (número entero): "))
-
-    texto_cifrado = cifrar_cesar(texto_original, correccion)
-
-    print("Texto cifrado:", texto_cifrado)
+            mensaje_cifrado += caracter
+    return mensaje_cifrado
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) != 3:
+        print("Uso: python3 cesar.py <mensaje> <corrimiento>")
+        sys.exit(1)
+
+    mensaje = sys.argv[1]
+    corrimiento = int(sys.argv[2])
+
+    mensaje_cifrado = cifrar_cesar(mensaje, corrimiento)
+    print("Mensaje cifrado:", mensaje_cifrado)
